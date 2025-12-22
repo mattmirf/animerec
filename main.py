93b7c4bcdc53370002c83afbf0eb010d1f3efa51
Matthew Mirfield <Matthew.Mirfield@gmail.com>
2025-12-22 14:06:41 -0500

    Cleaned the data

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
 df = pd.read_csv("animes.csv")
-print(df["genres_detailed"])
+
+# Cleaning data
+df.drop(df[df["type"] == "ONA"].index, inplace=True)
+df.drop(df[df["type"] == "OVA"].index, inplace=True)
+df.drop(df[df["type"] == "SPECIAL"].index, inplace=True)
+
+df.drop(df[df["score"] == '?'].index, inplace=True)
+df["score"] = df["score"].astype(float)
+df.drop(df[df["score"] < 7].index, inplace=True)
+
+df.drop(df[df["year"] == '?'].index, inplace=True)
+df["year"] = df["year"].astype (int)
+
+df.drop(df[df["genres"] == "['Hentai']"].index, inplace=True) 
+
+df.to_csv("cleaned_animes.csv", index=False)
